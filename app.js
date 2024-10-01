8402783dcf790717509d771679631edae4ee5109
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-10-01 12:46:21 +0500

    added class practice of funtions and return

---
+++ app.js
@@ -0,0 +1,61 @@
+
+// var user = prompt(" Are you minahil's Jameel student");
+// user = user.toLowerCase()
+// if (user === "YES"){
+//     console.log("welcome");
+    
+// }
+
+// function tea(sugar, patti){
+//     var incredient = 3 + sugar + patti + 6
+//     console.log(incredient , "ban gae chai");
+    
+// }
+// tea(3,2)
+// tea(4,3)
+
+// function juice(water,sugar,ice,tang){
+//     var incredient= water + sugar + ice + tang;
+//     console.log(incredient, "Yummy juice");
+    
+// }
+// juice(3,4,10,6)
+
+// function info () \\ parameter{
+//     var userName = prompt("what is your name?")
+//     var age = prompt("How old are you?")
+//     var skill = prompt("which book do you read?")
+// console.log(`My Name is ${userName} . I am ${age} years old. I am reading ${skill} book`);
+
+// }
+// info() //arrgument
+
+// function biryani ( ing1, ing2 ,ing3 ){
+//    console.log(`i made biryani with ${ing1} and ${ing2} with ${ing3 }`);
+    
+// }
+// biryani(rice, yakhni, raita)
+  
+       //RETURN
+// koi bh cheez bahar lany k liye fuction ko return karwana parta h
+
+// function tea(){
+//     //milk + sugar + patti + glass
+//     var incredient = 3 + 5 + 1 + 6
+//     console.log(incredient , "ban gae chai");
+//     return incredient
+// }
+// var userWish = tea()
+// console.log(userWish);
+
+
+
+
+// function juice(water,sugar,ice,tang){
+//     var incredient= water + sugar + ice + tang;
+//     console.log(incredient, "Yummy juice");
+//     return incredient
+// }
+//  var user = juice(3,4,10,6) 
+//  console.log(user, " splendid taste");
+ 
